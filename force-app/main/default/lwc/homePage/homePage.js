6b88d09c9f7ca93d6895f6c68f1323e65e80d35b
amos <amos@saltboxmgmt.com>
2025-03-16 23:44:40 -0400

    updated scroller

--- force-app/main/default/lwc/homePage/homePage.js
+++ force-app/main/default/lwc/homePage/homePage.js
@@ -6,6 +6,7 @@ export default class HomePage extends NavigationMixin(LightningElement) {
     tmdbApiKey = tmdbApiKey;
 
     trendingUrl = `https://api.themoviedb.org/3/trending/movie/day?api_key=${this.tmdbApiKey}&language=en-USinclude_adult=false&certification_country=US`;
+    
     actionUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${this.tmdbApiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&certification_country=US&with_genres=28`;
 
     animationUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${this.tmdbApiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&certification_country=US&with_genres=16`;
