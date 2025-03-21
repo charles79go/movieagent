d4b904989e84628e5cee4a91d81b94d0f285e4cc
amos <amos@saltboxmgmt.com>
2025-03-21 14:09:50 -0400

    header banner home button

--- force-app/main/default/lwc/header/header.js
+++ force-app/main/default/lwc/header/header.js
@@ -1,5 +1,15 @@
 import { LightningElement, api } from 'lwc';
+import { NavigationMixin } from "lightning/navigation";
 
-export default class Header extends LightningElement {
+export default class Header extends NavigationMixin(LightningElement) {
     @api tmdbApiKey;
+
+    goToHomePageFn() {
+        this[NavigationMixin.Navigate]({
+            type: 'comm__namedPage',
+            attributes: {
+                name: 'Home',
+            }
+        });
+    }
 }
