69fe900bb5e661f194c28bea3e9b2a9f54c6bd80
amos <amos@saltboxmgmt.com>
2025-03-21 16:28:31 -0400

    search working

--- force-app/main/default/lwc/header/header.js
+++ force-app/main/default/lwc/header/header.js
@@ -4,6 +4,12 @@ import { NavigationMixin } from "lightning/navigation";
 export default class Header extends NavigationMixin(LightningElement) {
     @api tmdbApiKey;
 
+    query = '';
+
+    onChangeQueryFn(e) {
+        this.query = e.target.value
+    }
+
     goToHomePageFn() {
         this[NavigationMixin.Navigate]({
             type: 'comm__namedPage',
@@ -11,5 +17,27 @@ export default class Header extends NavigationMixin(LightningElement) {
                 name: 'Home',
             }
         });
+    }
+
+    goToSearchPageFn() {
+
+        if(this.query === '') return;
+
+        let sendQuery = this.query.trim();
+        this.query = '';
+
+        this[NavigationMixin.Navigate]({
+            type: 'comm__namedPage',
+            attributes: {
+                name: 'SearchPage__c',
+            },
+            state: {
+                searchQuery: sendQuery,
+            },
+        });
+    }
+
+    keyupFn(event) {
+        if(event.keyCode === 13) this.goToSearchPageFn();
     }
 }
---
+++ force-app/main/default/lwc/searchPage/searchPage.js
@@ -0,0 +1,58 @@
+import { LightningElement, api, wire} from 'lwc';
+import tmdbApiKey from "@salesforce/label/c.tmdbApiKey";
+import { CurrentPageReference } from 'lightning/navigation';
+import util from 'c/util';
+
+export default class SearchPage extends LightningElement {
+    tmdbApiKey = tmdbApiKey;
+
+    movieSearchResults = [];
+    personSearchResults = [];
+    dataReady = false;
+
+    imageBaseUrl = 'https://image.tmdb.org/t/p/original';
+
+    searchQuery = '';
+
+    @wire(CurrentPageReference)
+    currentPageRef(pageRef) {
+    //   console.log('Page name:', pageRef.state.searchQuery);
+      this.searchQuery = pageRef.state.searchQuery;
+
+      this.searchMoviesFn();
+    }
+
+    async searchMoviesFn(){
+
+        this.dataReady = false;
+
+        // console.log('one call', this.searchQuery)
+
+        try {
+            let movieSearchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${this.tmdbApiKey}&language=en-US&query=${this.searchQuery}&page=1&include_adult=false`;
+
+            let movieResponse = await fetch(movieSearchUrl);
+            let movieData = await movieResponse.json();
+            this.movieSearchResults = movieData.results;
+
+            let personSearchUrl = `https://api.themoviedb.org/3/search/person?api_key=${this.tmdbApiKey}&language=en-US&query=${this.searchQuery}&page=1&include_adult=false`;
+
+            let personResponse = await fetch(personSearchUrl);
+            let personData = await personResponse.json();
+            this.personSearchResults = personData.results;
+
+            this.dataReady = true;
+
+            // console.log('>>>>', JSON.stringify(this.movieSearchResults,null,2))
+            // console.log('>>>>', JSON.stringify(this.personSearchResults,null,2))
+
+
+            // console.log(this.movieSearchResults.results.length)
+            // console.log(this.personSearchResults.results.length)
+
+        } catch(e) {
+            console.log('error')
+        }
+    }
+
+}
