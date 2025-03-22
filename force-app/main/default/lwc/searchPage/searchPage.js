260fa902595410915026d3b9051ffe9db63fea1d
amos <amos@saltboxmgmt.com>
2025-03-21 20:59:54 -0400

    search people only with jpg

--- force-app/main/default/lwc/searchPage/searchPage.js
+++ force-app/main/default/lwc/searchPage/searchPage.js
@@ -32,7 +32,6 @@ export default class SearchPage extends LightningElement {
             let movieSearchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${this.tmdbApiKey}&language=en-US&query=${this.searchQuery}&page=1&include_adult=false`;
             // let movieSearchUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${this.tmdbApiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&certification_country=US&sort_by=popularity.desc&vote_count.gte=10&with_keywords=${this.searchQuery}&append_to_response=videos`;
 
-
             let movieResponse = await fetch(movieSearchUrl);
             let movieData = await movieResponse.json();
             this.movieSearchResults = movieData.results;
@@ -41,7 +40,9 @@ export default class SearchPage extends LightningElement {
 
             let personResponse = await fetch(personSearchUrl);
             let personData = await personResponse.json();
-            this.personSearchResults = personData.results;
+            this.personSearchResults = personData.results.filter(person => {
+                return person.profile_path?.endsWith('.jpg');
+            });
 
             this.dataReady = true;
 
